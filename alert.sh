fe559766f6c7f0456d5a4ec270ac37f2bf99993d
0Rhys <69161991+0Rhys@users.noreply.github.com>
2020-12-04 20:36:09 +0300

    Update alert.sh

--- alert.sh
+++ alert.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 # by : Twitter @8Rhys
 # under GPL
+# for use you have to Enable beep command in linux
+
+ cat << "EOF"
+       .-.-.
+    .'_....._'.		,------. ,--.  
+   / / .12 . \ \	|  .--. '|  ,---. ,--. ,--.,---.  
+  | | '  |  ' | |	|  '--'.'|  .-.  | \  '  /(  .-'  
+  | | 9  /  3 | |	|  |\  \ |  | |  |  \   ' .-'  `) 
+   \ \ '.6.' / /	`--' '--'`--' `--'.-'  /  `----' 
+    '.`-...-'.'		  twitter: @8Rhys `---'   
+      '-- --'       
+EOF
+now=$(date \+%H%M)
+echo -e "\e[32m Local Time is \e[41m $now \e[0m"
 
 echo "Type the alarm time in [ 0000 Hours&minutes ] :"
 read time1
 echo "Put an alert message ? "
-read mes
+read msg
 
 while ( true ) do
 
@@ -13,9 +27,16 @@ while ( true ) do
    then
 
      beep
-     xmessage -buttons "Okay" -nearmouse $mes -timeout 5
+     xmessage -buttons "Okay" -nearmouse $msg -timeout 30
 
    fi
 sleep 10
 
 done
+
+
+
+
+                                   
+                                   
+
